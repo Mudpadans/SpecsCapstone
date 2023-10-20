7d647f3e3a7ccccdd2c74e5ec1c59505dc9b7681
Mudpadans <philmodel.101@gmail.com>
2023-10-19 22:45:05 -0400

    final commit (maybe)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { UserContext } from './createContext';
 import './App.css'
 import { storage } from './localStorageUtil'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './Redux/store'
 
 import Footer from './Components/Footer';
 import Header from './Components/Header';
@@ -33,23 +35,25 @@ const App = () => {
     }, [userId])
 
     return (
-        <UserContext.Provider value={{ userId, setUserId }}>
-            <Router>
-                <div className='App'>
-                    <Header />
-                    <Routes>
-                        <Route path="/" element={<Home />}/>
-                        <Route path="/about" element={<About />}/>
-                        <Route path="/services" element={<Services />}/>
-                        <Route path="/contact" element={<Contact />}/>
-                        <Route path="/createAppointment" element={<CreateAppointment />}/>
-                        <Route path='/appointments' element={<Appointments />} />
-                        <Route path="/auth" element={<Auth />}/>
-                    </Routes>
-                    <Footer />
-                </div>
-            </Router>
-        </UserContext.Provider>
+        <Provider store={store}>
+            <UserContext.Provider value={{ userId, setUserId }}>
+                <Router>
+                    <div className='App'>
+                        <Header />
+                        <Routes>
+                            <Route path="/" element={<Home />}/>
+                            <Route path="/about" element={<About />}/>
+                            <Route path="/services" element={<Services />}/>
+                            <Route path="/contact" element={<Contact />}/>
+                            <Route path="/createAppointment" element={<CreateAppointment />}/>
+                            <Route path='/appointments' element={<Appointments />} />
+                            <Route path="/auth" element={<Auth />}/>
+                        </Routes>
+                        <Footer />
+                    </div>
+                </Router>
+            </UserContext.Provider>
+        </Provider>
     )
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,20 +2,27 @@ import React, { useState, useContext } from 'react'
 import { Link } from 'react-router-dom';    
 import { UserContext } from '../createContext';
 import { storage } from '../localStorageUtil'
+import { useDispatch, useSelector } from 'react-redux';
+import { startLoading, stopLoading } from '../Redux/Slices/isLoading'
 
 const Header = () => {
     const { userId, setUserId } = useContext(UserContext);
+    const dispatch = useDispatch()
+    const isLoading = useSelector((state) => state.loading.isLoading)
 
     const logoutHandler = () => {
+        dispatch(startLoading())
         storage.removeItem('token');
         storage.removeItem('user');
         setUserId(null);
         window.alert("You've successfully logged out!");
+        dispatch(stopLoading())
     }
 
     return (
         <header className='header'>
             <h1>GeneralCareClinic</h1>
+            {isLoading && <div>Loading...</div>}
             <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
